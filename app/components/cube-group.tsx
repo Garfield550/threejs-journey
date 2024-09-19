961c6a15b399bf5ccfa4acf78514e9d6e46d807e
Garfield Lee <Garfield550@users.noreply.github.com>
2024-09-19 16:01:14 +0800

    feat: 05. Animations

--- app/components/cube-group.tsx
+++ app/components/cube-group.tsx
@@ -1,12 +1,40 @@
-import { GroupProps } from "@react-three/fiber";
+import { GroupProps, useFrame, useThree } from "@react-three/fiber";
 import Box from "./box";
+import { Mesh } from "three";
+import { useRef } from "react";
 
 export default function CubeGroup(props: GroupProps) {
+    const cube1 = useRef<Mesh>(null);
+    const cube2 = useRef<Mesh>(null);
+    const cube3 = useRef<Mesh>(null);
+
+    const camera = useThree((state) => state.camera);
+    const clock = useThree((state) => state.clock);
+
+    useFrame(() => {
+        if (!cube1.current || !cube2.current || !cube3.current) return;
+
+        const elapsed = clock.getElapsedTime() * Math.PI * 2;
+
+        camera.position.y = Math.sin(elapsed);
+        camera.position.x = Math.cos(elapsed);
+        camera.lookAt(cube2.current.position);
+
+        cube1.current.position.x = Math.sin(elapsed);
+        cube2.current.position.z = Math.cos(elapsed);
+        cube3.current.position.y = Math.sin(elapsed);
+        cube3.current.position.x = Math.cos(elapsed);
+
+        cube1.current.rotation.x = elapsed;
+        cube2.current.rotation.z = elapsed;
+        cube3.current.rotation.y = elapsed;
+    })
+
     return (
         <group {...props}>
-            <Box position={[-2, 0, 0]} />
-            <Box position={[0, 0, 0]} />
-            <Box position={[2, 0, 0]} />
+            <Box position={[-2, 0, 0]} ref={cube1} />
+            <Box position={[0, 0, 0]} ref={cube2} />
+            <Box position={[2, 0, 0]} ref={cube3} />
         </group>
     )
 }
--- app/components/scene.tsx
+++ app/components/scene.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Stats } from '@react-three/drei';
 import { Canvas } from "@react-three/fiber";
 import Axes from "./axes";
 import CubeGroup from "./cube-group";
@@ -11,7 +12,8 @@ export default function Scene() {
             <ambientLight intensity={Math.PI / 2} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
             <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-            <CubeGroup position={[0, 1, 0]} scale={[1, 2, 1]} rotation={[0, 1, 0]} />
+            <CubeGroup />
+            <Stats />
         </Canvas>
     )
 }
