26c231675764a4d400cb108203e538520e0a7402
Garfield Lee <Garfield550@users.noreply.github.com>
2024-09-22 06:08:39 +0800

    feat: 08. Geometries

--- app/components/box.tsx
+++ app/components/box.tsx
@@ -2,7 +2,10 @@
 
 import { MeshProps } from "@react-three/fiber"
 import { forwardRef, useCallback, useState } from "react"
-import { Mesh } from "three"
+import { BufferAttribute, Mesh } from "three"
+
+const positions = new Float32Array(5000 * 3 * 3).map(() => (Math.random() - 0.5) * 5)
+const positionsAttribute = new BufferAttribute(positions, 3)
 
 const Box = forwardRef<Mesh, MeshProps>(function Box(props, ref) {
     const [hovered, setHovered] = useState(false)
@@ -20,8 +23,11 @@ const Box = forwardRef<Mesh, MeshProps>(function Box(props, ref) {
             onClick={handleClick}
             onPointerOver={handlePointerOver}
             onPointerOut={handlePointerOut}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            {/* <boxGeometry args={[1, 1, 1, 2, 2, 2]} /> */}
+            <bufferGeometry>
+                <bufferAttribute attach="attributes-position" {...positionsAttribute} />
+            </bufferGeometry>
+            <meshStandardMaterial wireframe color={hovered ? 'hotpink' : 'orange'} />
         </mesh>
     )
 })
