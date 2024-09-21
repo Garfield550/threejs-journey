e1ae7c954e41992c48007d56bfabf123685c5272
Garfield Lee <Garfield550@users.noreply.github.com>
2024-09-22 04:43:40 +0800

    feat: 06. Cameras

--- app/components/scene.tsx
+++ app/components/scene.tsx
@@ -1,19 +1,20 @@
 'use client'
 
-import { Stats } from '@react-three/drei';
+import { OrbitControls, Stats } from '@react-three/drei';
 import { Canvas } from "@react-three/fiber";
 import Axes from "./axes";
-import CubeGroup from "./cube-group";
+import Box from './box';
 
 export default function Scene() {
     return (
         <Canvas>
+            <Stats />
             <Axes />
+            <OrbitControls enableDamping />
             <ambientLight intensity={Math.PI / 2} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
             <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-            <CubeGroup />
-            <Stats />
+            <Box position={[0, 0, 0]} />
         </Canvas>
     )
 }
