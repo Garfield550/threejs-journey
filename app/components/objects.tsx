e273fd7c07ffe6057d04a0140b42c537684e781b
Garfield Lee <Garfield550@users.noreply.github.com>
2024-09-28 17:48:40 +0800

    feat: 12. 3D Text

--- app/components/objects.tsx
+++ app/components/objects.tsx
@@ -1,99 +1,56 @@
-import { Environment, Plane, Sphere, Torus, useTexture } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { Fragment, useRef } from "react";
-import { Mesh, NearestFilter, Vector2 } from "three";
+import { Center, FontData, Text3D, Torus, useFont, useTexture } from "@react-three/drei";
+import { Fragment } from "react";
 
-import doorColor from '../textures/door/color.jpg'
-import doorAlpha from '../textures/door/alpha.jpg'
-import doorHeight from '../textures/door/height.jpg'
-import doorNormal from '../textures/door/normal.jpg'
-import doorRoughness from '../textures/door/roughness.jpg'
-import doorMetalness from '../textures/door/metalness.jpg'
-import doorAmbientOcclusion from '../textures/door/ambientOcclusion.jpg'
+import droidSans from '../fonts/droid/droid_sans_regular.typeface.json' with { type: 'json' };
 
-import matcapTexture from '../textures/matcaps/3.png'
-import gradientTexture from '../textures/gradients/3.jpg'
+import matcapTexture from '../textures/matcaps/1.png'
+
+const object100 = new Array(100).fill(null)
 
 export default function Objects() {
-  const sphere = useRef<Mesh>(null)
-  const plane = useRef<Mesh>(null)
-  const torus = useRef<Mesh>(null)
-
-  useFrame((_, delta) => {
-    if (!sphere.current || !plane.current || !torus.current) return
-
-    sphere.current.rotation.y += 0.1 * delta
-    plane.current.rotation.y += 0.1 * delta
-    torus.current.rotation.y += 0.1 * delta
-
-    sphere.current.rotation.x -= 0.15 * delta
-    plane.current.rotation.x -= 0.15 * delta
-    torus.current.rotation.x -= 0.15 * delta
-  })
-
-  const door = useTexture({
-    map: doorColor.src,
-    alphaMap: doorAlpha.src,
-    displacementMap: doorHeight.src,
-    normalMap: doorNormal.src,
-    roughnessMap: doorRoughness.src,
-    metalnessMap: doorMetalness.src,
-    aoMap: doorAmbientOcclusion.src
-  },
-    // It looks like useTexture use the right color space
-    // (textures) => {
-    //   textures.map.colorSpace = SRGBColorSpace
-    // }
-  )
-
+  const droidFont = useFont(droidSans as unknown as FontData)
   const matcap = useTexture(matcapTexture.src)
-
-  const gradient = useTexture(gradientTexture.src, (texture) => {
-    texture.minFilter = NearestFilter
-    texture.magFilter = NearestFilter
-    texture.generateMipmaps = false
-  })
 
   return (
     <Fragment>
-      <Environment files='/environmentMap/2k.hdr' background />
-      <Sphere ref={sphere} args={[0.5, 64, 64]} position={[-3, 0, 0]}>
-        <meshMatcapMaterial matcap={matcap} />
-      </Sphere>
-      <Sphere ref={sphere} args={[0.5, 64, 64]} position={[-1.5, 0, 0]}>
-        <meshPhysicalMaterial
-          transparent
-          metalness={0}
-          roughness={0}
-          transmission={1}
-          ior={1.5}
-          thickness={0.5}
-        />
-      </Sphere>
-      <Plane ref={plane} args={[1, 1, 100, 100]} position={[0, 0, 0]}>
-        {/* <meshStandardMaterial
-          {...door}
-          transparent
-          metalness={1}
-          roughness={1}
-          aoMapIntensity={1}
-          displacementScale={0.1}
-          normalScale={new Vector2(0.5, 0.5)}
-        /> */}
-        <meshPhysicalMaterial
-          {...door}
-          transparent
-          metalness={1}
-          roughness={1}
-          aoMapIntensity={1}
-          displacementScale={0.1}
-          normalScale={new Vector2(0.5, 0.5)}
-        />
-      </Plane>
-      <Torus ref={torus} args={[0.3, 0.2, 64, 128]} position={[1.5, 0, 0]}>
-        {/* <meshPhongMaterial shininess={100} specular={new Color(0x1188ff)} /> */}
-        <meshToonMaterial gradientMap={gradient} />
-      </Torus>
+      <Center>
+        <Text3D
+          font={droidFont.data}
+          size={0.5}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.03}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          Text 3D
+          <meshMatcapMaterial matcap={matcap} />
+        </Text3D>
+      </Center>
+      {object100.map((_, index) => {
+        const scale = Math.random()
+        return (
+          <Torus
+            key={index}
+            args={[0.3, 0.2, 20, 45]}
+            position={[
+              (Math.random() - 0.5) * 10,
+              (Math.random() - 0.5) * 10,
+              (Math.random() - 0.5) * 10
+            ]}
+            rotation={[
+              Math.random() * Math.PI,
+              Math.random() * Math.PI,
+              0
+            ]}
+            scale={[scale, scale, scale]}
+          >
+            <meshMatcapMaterial matcap={matcap} />
+          </Torus>
+        )
+      })}
     </Fragment>
   )
 }
