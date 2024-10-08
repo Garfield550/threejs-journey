bfbb3af3e5c7b928041652fb9c9b21f77d5f3611
Garfield Lee <Garfield550@users.noreply.github.com>
2024-10-09 00:48:23 +0800

    feat: 17. Particles

--- app/components/objects.tsx
+++ app/components/objects.tsx
@@ -1,17 +1,38 @@
-import House from "./house";
-import Floor from "./floor";
-import Bushes from "./bushes";
-import Graves from "./graves";
-import Ghosts from "./ghosts";
+import { Points, useTexture } from "@react-three/drei";
+import { useRef } from "react";
+import { Points as ThreePoints } from "three";
+
+import particlesTexture from '../textures/particles/2.png'
+import { AdditiveBlending } from "three";
+import { useFrame } from "@react-three/fiber";
+
+const positions = new Float32Array(20000 * 3).map(() => (Math.random() - 0.5) * 10)
+const colors = new Float32Array(20000 * 3).map(() => Math.random())
 
 export default function Objects() {
+  const points = useRef<ThreePoints>(null)
+  const texture = useTexture(particlesTexture.src)
+
+  useFrame(({ clock }) => {
+    const et = clock.getElapsedTime()
+
+    if (points.current) {
+      for (let i = 0; i < positions.length / 3; i++) {
+        const i3 = i * 3
+        // const x = positions[i3]
+        const x = points.current.geometry.attributes.position.array[i3]
+        points.current.geometry.attributes.position.array[i3 + 1] = Math.sin(et + x)
+      }
+
+      points.current.geometry.attributes.position.needsUpdate = true
+    }
+  })
+
   return (
     <group>
-      <House />
-      <Floor />
-      <Bushes />
-      <Graves />
-      <Ghosts />
+      <Points positions={positions} colors={colors} ref={points}>
+        <pointsMaterial attach='material' size={0.1} sizeAttenuation transparent alphaMap={texture} depthWrite={false} blending={AdditiveBlending} vertexColors />
+      </Points>
     </group>
   )
 }
--- app/components/scene.tsx
+++ app/components/scene.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { OrbitControls, PerspectiveCamera, Sky, Stats } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera, Stats } from '@react-three/drei';
 import { Canvas } from "@react-three/fiber";
 import Axes from "./helpers/axes";
 import Objects from './objects';
-import Lights from './lights';
 
 export default function Scene() {
   return (
@@ -12,17 +11,7 @@ export default function Scene() {
       <Stats />
       <Axes />
       <OrbitControls enableDamping />
-      <PerspectiveCamera makeDefault fov={75} position={[4, 2, 5]} near={0.1} far={100} />
-      <Sky
-        turbidity={10}
-        rayleigh={3}
-        mieCoefficient={0.1}
-        mieDirectionalG={0.95}
-        sunPosition={[0.3, -0.038, -0.95]}
-      />
-      {/* <fog attach="fog" color={'#ff0000'} near={10} far={13} /> */}
-      <fogExp2 attach="fog" color={'#04343f'} density={0.1} />
-      <Lights />
+      <PerspectiveCamera makeDefault fov={75} position={[0, 0, 3]} near={0.1} far={100} />
       <Objects />
     </Canvas>
   )
