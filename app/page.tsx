066be7e0f879d1a73cb0bf007cfc70db22503914
Garfield Lee <Garfield550@users.noreply.github.com>
2025-01-31 18:28:26 +0800

    feat: upgrade to tailwindcss v4

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Scene from "./components/scene";
 
 export default function Home() {
   return (
-    <main className="absolute inset-0 outline-none bg-black">
+    <main className="absolute inset-0 outline-hidden bg-black">
       <Scene />
     </main>
   );
